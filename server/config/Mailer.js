d9b4749bb780192fbe9d92f7ed994aaacbced212
Ahmad Abdullah Hashmi <ahmadhashmi1304@gmail.com>
2026-07-01 22:32:17 +0530

    Resolved All the bugs

--- server/config/Mailer.js
+++ server/config/Mailer.js
@@ -2,7 +2,8 @@ const nodemailer = require("nodemailer");
 const dns = require("dns");
 
 const SMTP_HOST = process.env.SMTP_HOST || "smtp.gmail.com";
-const SMTP_PORT = Number(process.env.SMTP_PORT) || 465;
+const SMTP_PORT = Number(process.env.SMTP_PORT) || 587;
+const SMTP_SECURE = SMTP_PORT === 465;
 
 const getIPv4SocketOptions = (options, callback) => {
   dns.resolve4(SMTP_HOST, (error, addresses) => {
@@ -24,7 +25,8 @@ const getIPv4SocketOptions = (options, callback) => {
 const transporter = nodemailer.createTransport({
   host: SMTP_HOST,
   port: SMTP_PORT,
-  secure: SMTP_PORT === 465,
+  secure: SMTP_SECURE,
+  requireTLS: !SMTP_SECURE,
   connectionTimeout: 15000,
   greetingTimeout: 15000,
   socketTimeout: 20000,
