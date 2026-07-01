4249392796fdaed9462d6959cd0a05753e48cd5f
Ahmad Abdullah Hashmi <ahmadhashmi1304@gmail.com>
2026-07-01 20:11:04 +0530

    Resolved a bug

--- server/server.js
+++ server/server.js
@@ -24,7 +24,13 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 app.use(express.json());
 
 app.get('/health', (req, res) => {
