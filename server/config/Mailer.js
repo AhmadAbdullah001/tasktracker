4f3a0f3722ed242d07705f4b9b02fe1c4f24b42b
Ahmad Abdullah Hashmi <ahmadhashmi1304@gmail.com>
2026-07-01 21:08:14 +0530

    Changed Some thing

--- server/config/Mailer.js
+++ server/config/Mailer.js
@@ -1,20 +1,11 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
-  family: 4,
-  connectionTimeout: 5000,
-  greetingTimeout: 5000,
-  socketTimeout: 5000,
+  service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER || '',
-    pass: process.env.EMAIL_PASS || '',
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
-  requireTLS: true,
-  logger: false,
-  debug: false,
 });
 
 module.exports = transporter;
